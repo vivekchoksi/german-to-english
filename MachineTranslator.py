03d82cdbdd2ba08ebd4e465cfb6967190d827fad
Vivek Choksi <viv.choksi@gmail.com>
2014-02-21 17:47:33 -0800

    Initial commit of MachineTranslator.py

---
+++ MachineTranslator.py
@@ -0,0 +1,37 @@
+import sys
+
+class MachineTranslator:
+  def __init__(self, filename):
+    self.filename = filename
+    pass
+
+  class PreProcessor:
+    def __init__(self):
+      pass
+
+    def pre_process(self, sentence):
+      pass
+
+  class PostProcessor:
+    def __init__(self):
+      pass
+
+    def post_process(self, sentence):
+      pass
+
+  def translate(self, sentence):
+    pass
+
+
+def main():
+  if len(sys.argv) != 2:
+    print 'MachineTranslator.py: expects 1 argument...'
+    print '\tUsage: python MachineTranslator.py <corpus-to-translate>'
+    sys.exit()
+
+  filename = sys.argv[1]
+  mt = MachineTranslator(filename)
+  pass
+
+if __name__ == "__main__":
+  main()
