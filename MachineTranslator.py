7a10ce94aabee702e5d2d66cfa140d272970ab89
Arjun Mathur <arjun@arjun-VirtualBox.(none)>
2014-02-25 17:02:24 -0800

    Basic Machine Translator done

--- MachineTranslator.py
+++ MachineTranslator.py
@@ -6,19 +6,29 @@ dictionary = eval(open('dictionary.txt').read())
 class MachineTranslator:
   def __init__(self, filename):
     self.filename = filename
-    self.tokenized_sentences = extract_tokenized_sentences(filename)    
-    self.tokenized_translations = direct_translate(self.tokenized_sentences)
-    import pdb; pdb.set_trace()
   
-  def direct_translate(self, tokenized_sentences):
+  def translate(self, outfile):
+    self.tokenized_sentences = self._extract_tokenized_sentences(self.filename)    
+    self.tokenized_translations = self._direct_translate(self.tokenized_sentences)
+    self._save_translations(self.tokenized_translations, outfile)
+
+  def _save_translations(self, tokenized_translations, outfile):
+    '''Saves our translations to outfile'''
+    with open(outfile, 'w') as f:
+      for sentence in tokenized_translations:
+        f.write(' '.join(sentence))
+        f.write('\n')
+
+  
+  def _direct_translate(self, tokenized_sentences):
     '''Translates a list of tokenized german sentences word-by-word'''
     result = []
     for sentence in tokenized_sentences:
-      translation = [dictionary[word]['translations'][0] for word in tokenized_sentence]
+      translation = [dictionary[word.lower()]['box_translation'] for word in sentence]
       result.append(translation)
     return result
 
-  def extract_tokenized_sentences(self, filename):
+  def _extract_tokenized_sentences(self, filename):
     '''Extracts a list of tokenized sentences from a file'''
     result = []
     with open(filename) as f:
@@ -47,13 +57,15 @@ class MachineTranslator:
 
 
 def main():
-  if len(sys.argv) != 2:
-    print 'MachineTranslator.py: expects 1 argument...'
-    print '\tUsage: python MachineTranslator.py <corpus-to-translate>'
+  if len(sys.argv) != 3:
+    print 'MachineTranslator.py: expects 2 argument...'
+    print '\tUsage: python MachineTranslator.py <corpus-to-translate> <translation-outfile>'
     sys.exit()
 
   filename = sys.argv[1]
+  outfile = sys.argv[2]
   mt = MachineTranslator(filename)
+  mt.translate(outfile)
   pass
 
 if __name__ == "__main__":
